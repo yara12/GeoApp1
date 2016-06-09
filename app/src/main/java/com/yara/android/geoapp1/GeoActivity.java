8867bc0becfc9dfbc2951c8fee71467dee92651e
Yara Jazmin <yara.jazmin@itcampeche.edu.mx>
2016-06-08 22:28:44 -0500

    version con demostracion de manejo de eventos del proyecto

--- app/src/main/java/com/yara/android/geoapp1/GeoActivity.java
+++ app/src/main/java/com/yara/android/geoapp1/GeoActivity.java
@@ -2,12 +2,36 @@ package com.yara.android.geoapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GeoActivity extends AppCompatActivity {
+    private Button mBotonCierto;
+    private Button mBotonFalso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
+
+        mBotonCierto =(Button) findViewById(R.id.boton_cierto);
+        mBotonFalso =(Button) findViewById(R.id.boton_falso);
+
+        mBotonCierto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoActivity.this, R.string.texto_correcto, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        mBotonFalso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoActivity.this, R.string.texto_incorrecto, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
     }
 }
